a79d46e5cd0e0e570b782aa1aea7bdea8bdb7bbf
Akash <akashriaz05@gmail.com>
2023-05-26 03:08:17 +0600

    marvelous-toy server code

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bsin5j.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+     client.connect();
+     const toyCollection = client.db('actionToysDB').collection('toys')
+
+     app.get('/allToys', async(req,res)=>{
+      const limit = parseInt(req.query.limit) || 20; 
+        const cursor = toyCollection.find().limit(limit);
+        const result = await cursor.toArray()
+        res.send(result);
+     })
+
+     app.get('/allToys/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(filter)
+        res.send(result);
+     })
+
+     app.post('/allToy',async(req,res)=>{
+        const toysInfo = req.body;
+        const result = await toyCollection.insertOne(toysInfo)
+        console.log(result)
+    })
+
+     app.get('/allToy', async(req,res)=>{
+        const sort = req.query.sort === 'desc' ? -1 : 1;
+        let query ={}
+        if(req.query?.email){
+            query={email: req.query.email}
+        }
+        const result =await toyCollection.find(query).toArray()
+        // const result =await toyCollection.find(query).sort({ price: sort }).toArray()
+        const sortedResult = result.sort((a, b) => sort * (parseFloat(a.price) - parseFloat(b.price)));
+
+
+        // const sortedResult = result.map((toy) => ({
+        //     ...toy,
+        //     price: parseInt(toy.price),
+        //   }));
+        
+
+        res.send(sortedResult)
+     })
+     app.get('/allToy/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(query)
+        res.send(result)
+     })
+
+     app.put('/allToy/:id', async(req,res)=>{
+        const id =req.params.id;
+        const updatedToy = req.body;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true };
+        const toy = {
+            $set: {
+              price:updatedToy.price,
+              quantity:updatedToy.quantity,
+              description:updatedToy.description
+            },
+          };
+          const result = await toyCollection.updateOne(filter, toy,options)
+          res.send(result)
+     })
+
+   
+    app.delete('/allToy/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.deleteOne(query)
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/',(req, res)=>{
+    res.send('the server is running test')
+})
+
+app.listen(port, ()=>{
+    console.log('the server is running on port',port)
+})
